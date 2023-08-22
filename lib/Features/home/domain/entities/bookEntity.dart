05a390bc3f1d3926e7f1493e4b5498684ff1ebe1
aya essam <ayaessam>
2023-08-22 04:17:07 +0300

    genrate book entity adapter

--- lib/Features/home/domain/entities/bookEntity.dart
+++ lib/Features/home/domain/entities/bookEntity.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+part 'bookEntity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookid;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final String? rating;
 
   BookEntity(
